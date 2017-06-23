a6baf3721f8cd54a6a26137cefb9e8d17a2f865e
giduturi <giduturisarath@gmail.com>
2017-06-23 15:21:14 +0530

    sampletesting

---
+++ ntp/manifests/apache.pp
@@ -0,0 +1,21 @@
+# Class: 
+#
+#
+class test::apache {
+    # resources
+
+    $package_name = $facts['os']['family'] ? {
+        'RedHat' => 'httpd',
+        'Debian' => 'apache2'
+    }
+
+    package { $package_name :
+        ensure => installed,
+        
+    }
+
+    service { $package_name:
+        ensure     => running,
+        enable     => true,
+    }
+}
